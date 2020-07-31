a287fbb2ca4e42ea43fa601096866100c0257210
sam_chong <sam_chong@mail.chungyo.net>
2020-07-31 16:52:10 +0800

    update file

--- 0731-master/Demo/Lab_LongRequest/LongRequest_Client.php
+++ 0731-master/Demo/Lab_LongRequest/LongRequest_Client.php
@@ -9,10 +9,22 @@ Test: <input type="text" id="txtTest" />
 <hr>
 <div id="debug"></div>
 <script>
+    var data = " \r\n";
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "LongRequest_Server.php", true);
     xhr.onprogress = function (e) {
-        document.getElementById("debug").innerHTML = xhr.responseText;
+        // document.getElementById("debug").innerHTML = xhr.responseText;
+
+        responseText = xhr.responseText;
+        lastEvent = responseText.replace(data, "");
+        //document.getElementById("debug").innerHTML = lastEvent;
+
+        let obj = JSON.parse(lastEvent); 
+        document.getElementById("debug").innerHTML = obj.data + "%"; // my code
+
+        //document.getElementById("debug").innerHTML = JSON.parse(responseText.replace(data, "")).data + "%";
+
+        data = responseText;
     }
     xhr.send(null);
 </script>
